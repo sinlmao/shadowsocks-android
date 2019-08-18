1a5e3ee4660b0422b9389fb57e2f934c5510c278
pexcn <i@pexcn.me>
2019-08-18 20:12:33 +0800

    refine code

--- generate.sh
+++ generate.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
 CUR_DIR=$(pwd)
-TMP_DIR=$(mktemp -d /tmp/gen.XXXXXX)
+TMP_DIR=$(mktemp -d /tmp/acl.XXXXXX)
 
 GFWLIST_URL="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
 CHNROUTE_URL="https://pexcn.me/daily/chnroute/chnroute.txt"
 
 function fetch_data() {
   cd $TMP_DIR
-  curl -sSL $GFWLIST_URL > gfwlist.txt
-  curl -sSL $CHNROUTE_URL > chnroute.txt
+  curl -sSL --connect-timeout 10 $GFWLIST_URL -o gfwlist.txt
+  curl -sSL --connect-timeout 10 $CHNROUTE_URL -o chnroute.txt
   cd $CUR_DIR
 }
 
@@ -26,7 +26,7 @@ function gen_gfwlist_acl() {
 function gen_chinalist_acl() {
   cd $TMP_DIR
   cp $CUR_DIR/template/china-list.acl .
-  sed -i "s/___CHNROUTE_LIST_PLACEHOLDER___/cat chnroute.txt/e" china-list.acl
+  sed -i "s/___CHNROUTE_PLACEHOLDER___/cat chnroute.txt/e" china-list.acl
   cd $CUR_DIR
 }
 
@@ -35,18 +35,18 @@ function gen_bypass_acls() {
   cp $CUR_DIR/template/bypass-china.acl .
   cp $CUR_DIR/template/bypass-lan-china.acl .
   sed -e "1,/proxy_list/d" gfwlist.acl > proxylist.txt
-  sed -i "s/___CHNROUTE_LIST_PLACEHOLDER___/cat chnroute.txt/e" bypass-china.acl bypass-lan-china.acl
-  sed -i "s/___GFWLIST_PROXY_LIST_PLACEHOLDER___/cat proxylist.txt/e" bypass-china.acl bypass-lan-china.acl
+  sed -i "s/___CHNROUTE_PLACEHOLDER___/cat chnroute.txt/e" bypass-china.acl bypass-lan-china.acl
+  sed -i "s/___GFWLIST_PLACEHOLDER___/cat proxylist.txt/e" bypass-china.acl bypass-lan-china.acl
   cd $CUR_DIR
 }
 
 function dist_release() {
-  mkdir -p gh-pages/acl
-  cp $TMP_DIR/gfwlist.acl gh-pages/acl/
-  cp $TMP_DIR/china-list.acl gh-pages/acl/
-  cp $TMP_DIR/bypass-china.acl gh-pages/acl/
-  cp $TMP_DIR/bypass-lan-china.acl gh-pages/acl/
-  cp template/bypass-lan.acl gh-pages/acl/
+  mkdir -p release/acl
+  cp $TMP_DIR/gfwlist.acl release/acl/
+  cp $TMP_DIR/china-list.acl release/acl/
+  cp $TMP_DIR/bypass-china.acl release/acl/
+  cp $TMP_DIR/bypass-lan-china.acl release/acl/
+  cp template/bypass-lan.acl release/acl/
 }
 
 fetch_data
